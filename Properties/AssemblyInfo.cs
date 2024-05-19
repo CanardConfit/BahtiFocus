24aeacb598a3997af181d8d0f9ed7102412f2f5d
CanardConfit <canardconfit.developpement@gmail.com>
2024-05-19 23:38:56 +0200

    Add License, logo, target nina version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("Homepage", "https://github.com/CanardConfit/BahtiFocus")]
 
 //[Optional] Common tags that quickly describe your plugin
-[assembly: AssemblyMetadata("Tags", "")]
+[assembly: AssemblyMetadata("Tags", "Focus,Focus Analyser,Bahtinov Mask")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
 [assembly: AssemblyMetadata("ChangelogURL", "https://github.com/CanardConfit/BahtiFocus/CHANGELOG.md")]
